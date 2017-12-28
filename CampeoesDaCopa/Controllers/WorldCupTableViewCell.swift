9f439b1b766e550fceb7b8c0863adea9f542b252
Ricardo Corrales <ricardocorrales@Mac-mini-de-Ricardo.local>
2017-12-28 17:01:00 -0200

    first commit

---
+++ CampeoesDaCopa/Controllers/WorldCupTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  WorldCupTableViewCell.swift
+//  CampeoesDaCopa
+//
+//  Created by Ricardo Corrales on 28/12/17.
+//  Copyright © 2017 Ricardo Corrales. All rights reserved.
+//
+
+import UIKit
+
+class WorldCupTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbYear: UILabel!
+    @IBOutlet weak var ivWinner: UIImageView!
+    @IBOutlet weak var ivVice: UIImageView!
+    @IBOutlet weak var lbWinner: UILabel!
+    @IBOutlet weak var lbVice: UILabel!
+    @IBOutlet weak var lbWinnerScore: UILabel!
+    @IBOutlet weak var lbViceScore: UILabel!
+    @IBOutlet weak var lbCountry: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func prepare(with cup: WorldCup) {
+        lbYear.text = "\(cup.year)"
+        ivWinner.image = UIImage(named: cup.winner)
+        ivVice.image = UIImage(named: cup.vice)
+        lbWinner.text = cup.winner
+        lbVice.text = cup.vice
+        lbCountry.text = cup.country
+        lbWinnerScore.text = cup.winnerScore
+        lbViceScore.text = cup.viceScore
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
